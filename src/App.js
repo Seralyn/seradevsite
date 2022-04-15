811c15050585463ddb95238b03e4bd2256985523
Seralyn Campbell <seralyncampbell@gmail.com>
2022-04-15 16:12:29 +0900

    added alt text to photos in hopes of fixing Netlify build and filled out more about me stuff

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Navbar from "./components/Navbar";
-import About from "./components/About";
-import Portfolio from "./components/Portfolio";
-import Knowledgebase from "./components/Knowledgebase";
-import LandingPage from "./components/LandingPage";
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Knowledgebase from './components/Knowledgebase';
+import LandingPage from './components/LandingPage';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/knowledgebase" element={<Knowledgebase />} />
-        </Routes>
-      </Router>
-      {/* <LandingPage /> */}
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/portfolio" element={<Portfolio />} />
+                    <Route path="/knowledgebase" element={<Knowledgebase />} />
+                </Routes>
+            </Router>
+            {/* <LandingPage /> */}
+        </div>
+    );
 }
 
 export default App;
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,19 +1,12 @@
-import React from "react";
-import "./LandingPage.css";
-import seraphoto from "./../img/seralyn01.jpeg";
-import logo from "../logo.svg";
+import React from 'react';
+import './LandingPage.css';
+
+import logo from '../logo.svg';
 
 export default function LandingPage() {
-  return (
-    <div className="flex flex-col items-center mt-12">
-      <div>
-        <img
-          className="rounded-lg shadow-2xl shadow-green-400 w-64"
-          src={seraphoto}
-        />
-      </div>
-      <p className="text-green-300 mt-6">Hi, I'm Seralyn.</p>
-      <img src={logo} className="App-logo" alt="logo" />
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center mt-12">
+            <img src={logo} className="App-logo" alt="logo" />
+        </div>
+    );
 }
