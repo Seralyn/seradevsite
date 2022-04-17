347f89493039fb0617c1e3b4a1088c3558073954
Seralyn Campbell <seralyncampbell@gmail.com>
2022-04-17 12:05:07 +0900

    Portfolio page layout and responsiveness achieved

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,39 +9,39 @@ import mikeDB from '../img/portfolioSiteThumbs/mikesDBThumb.png';
 
 export default function Portfolio() {
     return (
-        <div className="text-green-300 flex flex-col items-center">
-            <div className="w-80 mt-32">
-                <h1 className="text-3xl">Portfolio</h1>
+        <div className="text-green-300 flex flex-col">
+            <div className="flex flex-col justify-center mt-32">
+                <div className="">
+                    <h1 className="text-3xl">Portfolio</h1>
+                </div>
                 <div className="mt-10">
                     <p>
                         Here are a few examples of my work. Some are mock-ups,
                         some are functioning apps, SPAs, etc.
                     </p>
                 </div>
-                <div className="flex flex-wrap items-center justify-around mt-16">
-                    <div className="flex flex-col lg:flex-wrap lg:flex-row">
-                        <div className="">
-                            <img src={goodman} alt="" />
-                        </div>
-                        <div className="mt-14">
-                            <img src={foodsite} alt="" />
-                        </div>
-                        <div className="mt-14">
-                            <img src={weatherscrp} alt="" />
-                        </div>
-                        <div className="mt-14">
-                            <img src={letsconn} alt="" />
-                        </div>
-                        <div className="mt-14">
-                            <img src={guessNum} alt="" />
-                        </div>
-                        <div className="mt-14">
-                            <img src={mikeDB} alt="" />
-                        </div>
-                        <div className="mt-14">Item</div>
-                        <div className="mt-14">Item</div>
-                    </div>
+            </div>
+            <div className="inline-flex mt-20 justify-center flex-wrap">
+                <div className="w-96 mt-14 mx-5">
+                    <img src={goodman} alt="" />
                 </div>
+                <div className="w-96 mt-14 mx-5">
+                    <img src={foodsite} alt="" />
+                </div>
+                <div className="w-96 mt-14 mx-5">
+                    <img src={weatherscrp} alt="" />
+                </div>
+                <div className="w-96 mt-14 mx-5">
+                    <img src={letsconn} alt="" />
+                </div>
+                <div className="w-96 mt-14 mx-5">
+                    <img src={guessNum} alt="" />
+                </div>
+                <div className="w-96 mt-14 mx-5">
+                    <img src={mikeDB} alt="" />
+                </div>
+                <div className="w-96 mt-14 mx-5">Item</div>
+                <div className="w-96 mt-14 mx-5">Item</div>
             </div>
         </div>
     );
