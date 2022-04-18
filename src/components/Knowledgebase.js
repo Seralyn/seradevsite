4c6cec21c59a3be6155170c495e2ed600d477c19
Seralyn Campbell <seralyncampbell@gmail.com>
2022-04-18 12:00:20 +0900

    added and formatted logos

--- src/components/Knowledgebase.js
+++ src/components/Knowledgebase.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import './Knowledgebase.css';
+import bootstrap from '../img/frameworkLogos/bootstrapLogo.png';
+import gsap from '../img/frameworkLogos/greensockLogo.svg';
+import jquery from '../img/frameworkLogos/jqueryLogo.png';
+import nodejs from '../img/frameworkLogos/nodejs-new-pantone-black.svg';
+import react from '../img/frameworkLogos/reactLogo.png';
+import sass from '../img/frameworkLogos/sassLogo.png';
+import tailwind from '../img/frameworkLogos/tailwindcss-logotype.svg';
 
 export default function Knowledgebase() {
     // const frameworkImages = [
@@ -20,13 +27,46 @@ export default function Knowledgebase() {
 
     return (
         <div className="text-blue-300 flex flex-col items-center">
-            <div className="w-96 mt-32">
+            <div className="mt-32">
                 <h1 className="text-3xl">Knowledgebase</h1>
                 <div className="mt-10">
                     <p>
                         Although I have spent some time with Python, Java, &
                         Swift, my coding passion is mostly on the front-end.
                     </p>
+                </div>
+                <div
+                    id="framework-logo-container"
+                    className="flex gap-8 flex-wrap justify-evenly items-center p-20"
+                >
+                    <div>
+                        <img className="h-44 w-44" src={bootstrap} alt="" />
+                    </div>
+                    <div>
+                        <img className="w-96" src={tailwind} alt="" />
+                    </div>
+                    <div>
+                        <img className="w-96" src={jquery} alt="" />
+                    </div>
+                    <div>
+                        <img className="h-32 w-44" src={sass} alt="" />
+                    </div>
+                    <div>
+                        <img className="w-60" src={nodejs} alt="" />
+                    </div>
+                    <div>
+                        <img className="w-96" src={gsap} alt="" />
+                    </div>
+                    <div>
+                        <img className="w-60" src={react} alt="" />
+                    </div>
+                    <div>
+                        <img
+                            width="256"
+                            alt="AJAX logo by gengns"
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/AJAX_logo_by_gengns.svg/256px-AJAX_logo_by_gengns.svg.png"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
